34e3fdedcadf35ba19db685763cdf76a5d4d9566
Rubèn Nieto <ruben.np@me.com>
2019-10-05 11:23:03 +0200

    Cambiado Main para mostrar 2 modos de juego

--- src/com/trifulcas/id/Main.java
+++ src/com/trifulcas/id/Main.java
@@ -3,9 +3,15 @@ package com.trifulcas.id;
 public class Main {
 
 	public static void main(String[] args) {
-		Jugador eva = new Jugador("Eva", new Teclado(new Consola()));
+        Jugador eva = new Jugador("Eva", new Teclado(new Consola()));
         Jugador juan = new Jugador("Juan", new CPU());
         Juego ppt = new Juego(eva, juan, new JuegoPPTLS(), new Consola());
+        ppt.jugar();
+        
+        // otra opción...
+        eva = new Jugador("Eva", new Teclado());
+        juan = new Jugador("Juan", new CPU());
+        ppt = new Juego(eva, juan, new JuegoPPT());
         ppt.jugar();
 	}
 }
